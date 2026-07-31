fcd399cd6e920e55e7d550724dd097369d5c24cd
Bijay <bijay78082@gmail.com>
2026-07-31 14:14:08 +0530

    terraform

--- main.tf
+++ main.tf
@@ -1,8 +1,10 @@
-terraform {
-  backend "s3" {}
+module "components" {
+  source   = "git::https://github.com/bijaymunda181/tf-module-basic-test.git"
+  for_each = var.components
+
+  zone_id             = var.zone_id
+  security_groups     = var.security_groups
+  name                = each.value["name"]
+  instance_type       = each.value["instance_type"]
 }
 
-variable "test" {}
-output "test" {
-  value = var.test
-}
