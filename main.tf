2c0bf4362520ba7fef79ef349aeb20c6367b012a
Bijay <bijay78082@gmail.com>
2026-08-07 12:47:21 +0530

    terraform

--- main.tf
+++ main.tf
@@ -1,10 +1,8 @@
 module "components" {
-  source   = "git::https://github.com/bijaymunda181/tf-module-basic-test.git"
-  for_each = var.components
+  source   = "git::https://github.com/bijaymunda181/tf-module-vpc.git"
 
-  zone_id             = var.zone_id
-  security_groups     = var.security_groups
-  name                = each.value["name"]
-  instance_type       = each.value["instance_type"]
+  for_each = var.vpc
+  cidr = each.value["cidr"]
+
 }
 
